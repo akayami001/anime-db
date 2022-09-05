dd195ff8dd5b0685e0aa087302d3bbb09062969a
mehmet <beyzadetoprak@gmail.com>
2022-09-05 13:58:05 +0200

    re-sync

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then((res) => res.json());
+
+    setTopAnime(temp.top.slice(0, 10));
+  };
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    FetchAnime(search);
+  };
+
+  const FetchAnime = async (query) => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=descending&limit=10`
+    ).then((res) => res.json());
+
+    console.log(temp.results);
+
+    setAnimeList(temp.results);
+  };
+
+  useEffect(() => {
+    getTopAnime();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid anime-app">
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          setSearch={setSearch}
+          animeList={animeList}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header><h1>My<strong>Anime</strong>List <strong>Clone</strong></h1></header>
+  )
+}
